a882c785a9c818bc53d715b04e86171f54d3dfd4
holmdex <gustaf.holm99@gmail.com>
2025-02-27 22:46:11 +0100

    Create market-weather-indices.R

---
+++ market-weather-indices.R
@@ -0,0 +1,51 @@
+market_indices <- list(
+  US = c(
+    "^GSPC" = "S&P 500 | Area: United States",
+    "^DJI" = "Dow Jones | Area: United States",
+    "^IXIC" = "NASDAQ | Area: United States",
+    "^RUT" = "Russell 2000 | Area: United States",
+    "^VIX" = "VIX Volatility | Area: United States"
+  ),
+  
+  Asia = c(
+    "^N225" = "Nikkei 225 | Area: Japan",
+    "^HSI" = "Hang Seng | Area: Hong Kong",
+    "000001.SS" = "Shanghai Composite | Area: China Mainland",
+    "^KS11" = "KOSPI | Area: South Korea",
+    "^TWII" = "TAIEX | Area: Taiwan",
+    "^BSESN" = "BSE SENSEX | Area: India",
+    "^JKSE" = "Jakarta Composite | Area: Indonesia",
+    "^STI" = "Straits Times | Area: Singapore"
+  ),
+  
+  Europe = c(
+    "^FTSE" = "FTSE 100 | Area: United Kingdom",
+    "^GDAXI" = "DAX | Area: Germany",
+    "^FCHI" = "CAC 40 | Area: France",
+    "^STOXX50E" = "EURO STOXX 50 | Area: Eurozone",
+    "^IBEX" = "IBEX 35 | Area: Spain",
+    "^AEX" = "AEX | Area: Netherlands",
+    "^BFX" = "BEL 20 | Area: Belgium",
+    "^SSMI" = "Swiss Market | Area: Switzerland",
+    "^FTSEMIB.MI" = "FTSE MIB | Area: Italy"
+  ),
+  
+  Nordics = c(
+    "^OMXC25" = "OMX Copenhagen 25 | Area: Denmark",
+    "^OMXH25" = "OMX Helsinki 25 | Area: Finland",
+    "^OSEAX" = "Oslo Exchange | Area: Norway",
+    "^OMX" = "OMX Stockholm 30 | Area: Sweden"
+  ),
+  
+  Pacific = c(
+    "^AORD" = "All Ordinaries | Area: Australia",
+    "^NZ50" = "NZX 50 | Area: New Zealand"
+  ),
+  
+  Americas = c(
+    "^GSPTSE" = "S&P/TSX Composite | Area: Canada",
+    "^BVSP" = "Bovespa | Area: Brazil",
+    "^MXX" = "IPC Mexico | Area: Mexico",
+    "^MERV" = "MERVAL | Area: Argentina"
+  )
+)
